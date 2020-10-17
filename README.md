547c6550f1cf4726325b748c9c2fc93a038a5aa0
sushanth-34 <72566226+sushanth-34@users.noreply.github.com>
2020-10-17 20:50:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# class20
-collision detection
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+var fixedRect , movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 200, 80, 40);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2
+  && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
+  && movingRect.y-fixedRect.y<fixedRect.width/2+movingRect.width/2
+  && fixedRect.y-movingRect.y<fixedRect.width/2+movingRect.width/2){
+  fixedRect.shapeColor = "red";
+  movingRect.shapeColor = "red";
+}
+     else{
+      fixedRect.shapeColor = "green";
+      movingRect.shapeColor = "green";
+     }
+  drawSprites();
+
+
+}
